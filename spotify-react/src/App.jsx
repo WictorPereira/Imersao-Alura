7ecc8f0b2d3eb411bbcaa57093793e4376aa023f
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2024-01-31 09:08:35 -0300

    Componente Footer

--- spotify-react/src/App.jsx
+++ spotify-react/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 
 import Sidebar from './Components/Sidebar';
 import Main from'./Components/Main'
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div>
       <Sidebar/>
       <Main/>
+      <Footer/>
     </div>
     
  )
